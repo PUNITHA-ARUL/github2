6d3e702f916c0a87dfbef0a2e6b9b3d8e28e8310
PUNITHA-ARUL <arulpunitha2007@gmail.com>
2026-04-27 11:16:36 +0530

    Add input event handling for name field in Student Form

--- sample project/project3/script.js
+++ sample project/project3/script.js
@@ -1,1 +1,9 @@
- 
+var name=document.getElementById("name");
+var age=document.getElementById("age")
+//var gender=document.getElementById("female,male")
+
+function call(event){
+    var Name= name.textcontent("name")
+    console.log(Name.value)
+
+}
